800279f7fb40837d87e91603b2efefb0a8b9439a
Cam Prier <CSPrier@gmail.com>
2018-04-06 12:13:24 -0700

    corrected generateItemElement function and generateShoppingItemString function

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 'use strict';
 /* global $ */
 
-// `STORE` is responsible for storing the underlying data
-// that our app needs to keep track of in order to work.
-//
-// for a shopping list, our data model is pretty simple.
-// we just have an array of shopping list items. each one
-// is an object with a `name` and a `checked` property that
-// indicates if it's checked off or not.
-// we're pre-adding items to the shopping list so there's
-// something to see when the page first loads.
 const STORE = [
   {name: 'apples', checked: false},
   {name: 'oranges', checked: false},
@@ -17,10 +8,29 @@ const STORE = [
   {name: 'bread', checked: false}
 ];
 
+
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li>${item.name}</li>`;
+}
+
+
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
+
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  
+  return items.join('');
+}
+
+
 function renderShoppingList() {
-  // this function will be responsible for rendering the shopping list in
-  // the DOM
+  // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 
@@ -52,7 +62,6 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
-
 }
 
 // when the page loads, call `handleShoppingList`
